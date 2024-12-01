c61d5549c6166701bb26c5befdd129aefb194afe
acskii <andrew.sameh.06@gmail.com>
2024-12-01 16:08:10 +0200

    Solved sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (
+        (Number.isInteger(min) && Number.isInteger(max)) && 
+        (min > 0 && max > 0)
+    ) {
+            let sum = 0;
+            //const min = ((a > b) ? b : a);
+            //const max = ((a < b) ? b : a);
 
+            // Using deconstruction to swap variables
+            if (min > max) [min, max] = [max, min];
+
+            for (let i = min; i <= max; i++) {
+                sum += i;
+            }
+            return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
