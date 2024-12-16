d0d4bd9365b1ece9146bef5239dc988b863d3e1d
acskii <andrew.sameh.06@gmail.com>
2024-12-16 08:41:17 +0200

    Solved findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (person.hasOwnProperty('yearOfDeath')) {
+        return (person.yearOfDeath - person.yearOfBirth);
+    } else {
+        const date = new Date();
+        const currentYear = date.getFullYear();
 
+        return (currentYear - person.yearOfBirth);
+    }
+}
+
+const findTheOldest = function(people) {
+    const ages = people.map((person) => {
+        return {
+            name: person.name, 
+            age: getAge(person),
+        }
+    });
+
+    let max = ages[0];
+
+    ages.forEach((person) => {
+        if (person.age > max.age) {
+            max = person;
+        }
+    });
+
+    return max;
 };
 
 // Do not edit below this line
